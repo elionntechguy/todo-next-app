a2d5f67196e8bbebe2debdb6243826ce90459fb7
elionntechguy <shkololli40@gmail.com>
2022-06-10 07:14:32 +0200

    Add margin to todo outside container on mobile

--- components/Todo/index.tsx
+++ components/Todo/index.tsx
@@ -4,7 +4,7 @@ import TodoTasks from './TodoTasks';
 
 const Todo: FC = () => {
   return (
-    <div className="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top w-96">
+    <div className="relative px-7 py-6 mx-6 md:mx-0 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top">
       <div className="flex flex-col w-full">
         <TodoForm />
         <TodoTasks />
