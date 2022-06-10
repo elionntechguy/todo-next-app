ce843467fe68e661e6c8e8c3bd1c213419074204
elionntechguy <shkololli40@gmail.com>
2022-06-10 07:10:55 +0200

    Change options button text position

--- components/Todo/TodoOptions.tsx
+++ components/Todo/TodoOptions.tsx
@@ -7,7 +7,7 @@ const TodoOptions: FC = () => {
     <div className="top-16 text-right">
       <Menu as="div" className="relative inline-block">
         <div className="w-20 md:w-40">
-          <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-xs md:text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+          <Menu.Button className="inline-flex w-full justify-start rounded-md bg-black bg-opacity-20 px-4 py-2 text-xs md:text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
             Options
           </Menu.Button>
         </div>
